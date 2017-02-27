d51aac34855de28b746156433ca72a2b54b11cc5
yuvankarthik <karthikjune90@gmail.com>
2017-02-27 22:13:56 +0530

    Add files via upload

---
+++ www/js/config.js
@@ -0,0 +1,8 @@
+
+var krms_config ={	
+	'ApiUrl' : "YOUR_API_URL",
+	'DialogDefaultTitle' : "YOUR_OWN_DIALOG_TITLE",
+	'pushNotificationSenderid' : "YOUR_ANDROID_PUSH_PROJECT_ID",
+	'facebookAppId' : "YOUR_FACEBOOK_APP_ID",
+	'APIHasKey' : "YOUR API HASH KEY (OPTIONAL)"
+};
